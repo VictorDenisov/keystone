08ad827c321fbf7e124ced769ba3e085576b99ac
Victor Denisov <denisovenator@gmail.com>
2015-03-12 00:32:24 -0700

    Add a placeholder for user handler

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,30 @@
 module Main
 where
 
-import Web.Scotty (scotty, get, json)
+import Control.Monad (when)
+import Data.Maybe (isNothing)
+import qualified Data.Text.Lazy as T
+import qualified Web.Scotty as S
+import Network.HTTP.Types.Status (status401)
 import Version (apiV3, apiVersions)
 
-main = scotty 3000 $ do
-  get "/" $ do
-    json $ apiV3 "http://localhost" -- TODO It should be taken from the web server
-  get "/v3" $ do
-    json $ apiVersions "http://localhost" -- TODO It should be taken from the web server
+UserCreateRequestUser = UserCreateRequestUser { 
+
+UserCreateRequest = UserCreateRequest { userCreateRequestUser :: UserCreateRequestUser }
+
+main = S.scotty 3000 $ do
+  S.get "/" $ do
+    S.json $ apiV3 "http://localhost" -- TODO It should be taken from the web server
+  S.get "/v3" $ do
+    S.json $ apiVersions "http://localhost" -- TODO It should be taken from the web server
+  S.post "/v3/users" $ do
+    S.rescue (do
+        mToken <- S.header "X-Auth-Token"
+        when (isNothing mToken) $ S.raise "AUTH: Token is required"
+        let Just token = mToken
+        when (token /= "ADMIN_TOKEN") $ S.raise "AUTH: Wrong token"
+        S.text $ "Hello"
+      ) (\e -> do
+          when ((T.take 5 e) == "AUTH:") $ S.status status401
+          S.text (T.drop 5 e)
+        )
---
+++ src/User.hs
@@ -0,0 +1,3 @@
+module User
+where
+
