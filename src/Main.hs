7c5bac15bfcc00c920b5211975ed5c0a1d640136
Victor Denisov <vdenisov@mirantis.com>
2015-03-13 17:38:46 -0700

    Add parsing of UserCreateRequest

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main
 where
 
 import Control.Monad (when)
+import Control.Monad.IO.Class (liftIO)
 import Data.Maybe (isNothing)
 import qualified Data.Text.Lazy as T
 import qualified Web.Scotty as S
 import Network.HTTP.Types.Status (status401)
+import User (UserCreateRequest)
 import Version (apiV3, apiVersions)
-
-UserCreateRequestUser = UserCreateRequestUser { 
-
-UserCreateRequest = UserCreateRequest { userCreateRequestUser :: UserCreateRequestUser }
 
 main = S.scotty 3000 $ do
   S.get "/" $ do
@@ -24,7 +23,9 @@ main = S.scotty 3000 $ do
         when (isNothing mToken) $ S.raise "AUTH: Token is required"
         let Just token = mToken
         when (token /= "ADMIN_TOKEN") $ S.raise "AUTH: Wrong token"
-        S.text $ "Hello"
+        (d :: UserCreateRequest) <- S.jsonData
+        liftIO $ putStrLn $ show d
+        S.text $ "Hello" 
       ) (\e -> do
           when ((T.take 5 e) == "AUTH:") $ S.status status401
           S.text (T.drop 5 e)
--- src/User.hs
+++ src/User.hs
@@ -1,3 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
 module User
 where
 
+import Control.Monad (mzero)
+import Control.Applicative ((<*>), (<$>))
+import Data.Aeson (FromJSON(..), (.:), (.:?), Value(..))
+
+data UserCreateRequest = UserCreateRequest
+                       { projectId :: String
+                       , description :: String
+                       , domainId :: Maybe String
+                       , email :: String
+                       , enabled :: Bool
+                       , name :: String
+                       , password :: Maybe String
+                       } deriving Show
+
+instance FromJSON UserCreateRequest where
+  parseJSON (Object v) = do
+    user <- v .: "user"
+    UserCreateRequest
+        <$> (user .:  "default_project_id")
+        <*> (user .:  "description")
+        <*> (user .:? "domain_id")
+        <*> (user .:  "email")
+        <*> (user .:  "enabled")
+        <*> (user .:  "name")
+        <*> (user .:? "password")
+  parseJSON _ = mzero
