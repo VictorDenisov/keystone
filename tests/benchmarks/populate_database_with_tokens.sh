9043103a5e13994f8d816490a37173b4a5a5d7c5
Victor Denisov <denisovenator@gmail.com>
2015-09-05 15:02:11 -0700

    Migrate to mongodb driver 2.0.7

--- tests/benchmarks/populate_database_with_tokens.sh
+++ tests/benchmarks/populate_database_with_tokens.sh
@@ -2,7 +2,7 @@
 
 cd ../
 for i in `seq 1 7000`; do
-    ./auth_user.sh 5ce2297aba1f4f6f8afe12b542c9acc2 08fcddcf61774ce9bd0cc714fa5284ac admin
-#    ./auth_user.sh 5571eb76230019086e000001 5589884c2300191ce3000004 admin
+#    ./auth_user.sh 5ce2297aba1f4f6f8afe12b542c9acc2 08fcddcf61774ce9bd0cc714fa5284ac admin
+    ./auth_user.sh 5571eb76230019086e000001 5589884c2300191ce3000004 admin
 done
 cd benchmarks
