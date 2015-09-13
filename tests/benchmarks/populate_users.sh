74aff2848e9929d3d7e2a1ebcca2610e6a26aab8
Victor Denisov <denisovenator@gmail.com>
2015-09-13 02:32:49 -0700

    More benchmark results

--- tests/benchmarks/populate_users.sh
+++ tests/benchmarks/populate_users.sh
@@ -2,6 +2,10 @@
 
 cd ../
 
+# normal keystone
+#token=`./auth_user.sh 5ce2297aba1f4f6f8afe12b542c9acc2 08fcddcf61774ce9bd0cc714fa5284ac admin | grep X-Subject-Token | awk '{ print $2; }'`
+
+# my keystone
 token=`./auth_user.sh 5571eb76230019086e000001 5589884c2300191ce3000004 admin | grep X-Subject-Token | awk '{ print $2; }'`
 
 for i in `seq 1 7000`; do
