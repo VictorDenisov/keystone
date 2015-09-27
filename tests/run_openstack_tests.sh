76b735332dc48ef08f38a9a2316fbb6f2357f30d
Victor Denisov <denisovenator@gmail.com>
2015-09-27 16:17:48 -0700

    Add more openstack tests

--- tests/run_openstack_tests.sh
+++ tests/run_openstack_tests.sh
@@ -15,3 +15,22 @@ openstack service create --name cinder volume
 openstack endpoint create keystone public http://localhost:35357/v3
 openstack endpoint create keystone internal http://localhost:35357/v3
 openstack endpoint create keystone admin http://localhost:35357/v3
+
+openstack domain list
+
+openstack project create test_project_to_delete
+openstack project list
+openstack project show test_project_to_delete
+openstack project delete test_project_to_delete
+
+openstack role create test_role
+openstack role list
+openstack role show test_role
+openstack role delete test_role
+
+openstack role create admin
+
+openstack project create admin_project
+openstack user create --password admin_password --email admin_email@gmail.com --project admin_project admin_user
+
+openstack role add --project admin_project --user admin_user admin
