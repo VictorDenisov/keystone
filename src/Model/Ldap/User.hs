2a7eb654e52d6cd70281ed39379ff11a9160e9fb
Victor Denisov <denisovenator@gmail.com>
2015-12-15 11:34:49 -0800

    Add listing users from ldap

--- src/Model/Ldap/User.hs
+++ src/Model/Ldap/User.hs
@@ -1,8 +1,7 @@
 module Model.Ldap.User
 where
 
-import Control.Monad (forM_)
-import Data.Bson (genObjectId)
+import Data.Maybe (listToMaybe, catMaybes, fromMaybe)
 
 import qualified Config as C
 import qualified LDAP as L
@@ -11,11 +10,30 @@ import qualified Model.User as MU
 listUsers :: C.LdapConfig -> L.LDAP -> IO [MU.User]
 listUsers c l = do
   entries <- L.ldapSearch l (Just $ C.userTreeDn c) L.LdapScopeSubtree Nothing L.LDAPAllUserAttrs False
-  putStrLn $ "-----------"
-  forM_ entries $ \e -> do
-    putStrLn $ "Name is - " ++ (L.ledn e)
-    forM_ (L.leattrs e) $ \(attrName, attrValues) -> do
-      putStrLn $ "  " ++ attrName ++ " " ++ (show attrValues)
-  putStrLn $ "-----------"
-  oid <- genObjectId
-  return [MU.User oid (Just "Description") (Just "email") True "Simply Name" (Just "Password")]
+  let users = catMaybes $ map (entryToUser c) (filter (isObjectClass $ C.userObjectClass c) entries)
+  return users
+
+isObjectClass :: String -> L.LDAPEntry -> Bool
+isObjectClass oc e =
+  let mValues = lookup "objectClass" (L.leattrs e) in
+  case mValues of
+    Nothing     -> False
+    Just values -> elem oc values
+
+entryToUser :: C.LdapConfig -> L.LDAPEntry -> Maybe MU.User
+entryToUser c e = do
+  name <- getUserAttribute (C.userNameAttribute c) (L.leattrs e)
+  soid  <- getUserAttribute (C.userIdAttribute c) (L.leattrs e)
+  return (MU.User
+              (read soid)
+              (Just "Description")
+              (getUserAttribute (C.userMailAttribute c) (L.leattrs e))
+              True
+              (fromMaybe "" $ getUserAttribute (C.userNameAttribute c) (L.leattrs e))
+              (getUserAttribute (C.userPassAttribute c) (L.leattrs e))
+         )
+
+getUserAttribute :: String -> [(String, [String])] -> Maybe String
+getUserAttribute attribute leattrs = do
+  attrs <- lookup attribute leattrs
+  listToMaybe attrs
