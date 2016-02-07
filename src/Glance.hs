125491f9b673741b86afa1e3827c7466758b1bee
Victor Denisov <denisovenator@gmail.com>
2016-02-06 22:21:22 -0800

    Add metadefs schemas

--- src/Glance.hs
+++ src/Glance.hs
@@ -18,7 +18,13 @@ import Data.ByteString.Lazy.Char8 (unpack, pack)
 import Data.Time.Clock (getCurrentTime)
 import Glance.Config (confFileName, GlanceConfig(..))
 import Glance.Web.Version (listVersionsH)
-import Glance.Web.Schema (imagesSchemaH, imageSchemaH, metadefsNamespaceSchemaH)
+import Glance.Web.Schema ( imagesSchemaH, imageSchemaH
+                         , metadefsNamespaceSchemaH, metadefsResourceTypeSchemaH
+                         , metadefsNamespacesSchemaH, metadefsObjectsSchemaH
+                         , metadefsResourceTypesSchemaH, metadefsTagSchemaH
+                         , metadefsTagsSchemaH, metadefsObjectSchemaH
+                         , metadefsPropertySchemaH, metadefsPropertiesSchemaH
+                         )
 import Network.HTTP.Types.Status (statusCode, status500)
 import Network.Wai (Middleware, responseLBS)
 import Network.Wai.Handler.Warp (defaultSettings, setPort, runSettings)
@@ -79,11 +85,20 @@ application policy config = do
         S.json $ e {E.message = "Internal error. Server time - " ++ (show time)}
       _ -> do
         S.json e
-  S.get   "/"                              $ listVersionsH            config
-  S.get   "/versions"                      $ listVersionsH            config
-  S.get   "/v2/schemas/images"             $ imagesSchemaH            config
-  S.get   "/v2/schemas/image"              $ imageSchemaH             config
-  S.get   "/v2/schemas/metadefs/namespace" $ metadefsNamespaceSchemaH config
+  S.get   "/"                                   $ listVersionsH                config
+  S.get   "/versions"                           $ listVersionsH                config
+  S.get   "/v2/schemas/images"                  $ imagesSchemaH                config
+  S.get   "/v2/schemas/image"                   $ imageSchemaH                 config
+  S.get   "/v2/schemas/metadefs/namespace"      $ metadefsNamespaceSchemaH     config
+  S.get   "/v2/schemas/metadefs/namespaces"     $ metadefsNamespacesSchemaH    config
+  S.get   "/v2/schemas/metadefs/object"         $ metadefsObjectSchemaH        config
+  S.get   "/v2/schemas/metadefs/objects"        $ metadefsObjectsSchemaH       config
+  S.get   "/v2/schemas/metadefs/property"       $ metadefsPropertySchemaH      config
+  S.get   "/v2/schemas/metadefs/properties"     $ metadefsPropertiesSchemaH    config
+  S.get   "/v2/schemas/metadefs/resource_type"  $ metadefsResourceTypeSchemaH  config
+  S.get   "/v2/schemas/metadefs/resource_types" $ metadefsResourceTypesSchemaH config
+  S.get   "/v2/schemas/metadefs/tag"            $ metadefsTagSchemaH           config
+  S.get   "/v2/schemas/metadefs/tags"           $ metadefsTagsSchemaH          config
 
 verifyDatabase :: GlanceConfig -> IO ()
 verifyDatabase GlanceConfig{..} = return () -- TODO
--- src/Glance/Web/Schema.hs
+++ src/Glance/Web/Schema.hs
@@ -18,28 +18,67 @@ import qualified Web.Scotty.Trans as S
 
 imagesSchemaH :: (Functor m, MonadIO m) => GlanceConfig -> ActionM m ()
 imagesSchemaH c = do
-  images_schema <- liftIO $ LBS.readFile "images_schema.json"
-  case (decode images_schema :: Maybe Value) of
-    Nothing ->
-      throwError $ S.stringError "Failed  to parse images_schema.json file"
-    Just v ->
-      S.json v
+  handleSchema "images_schema.json"
 
 imageSchemaH :: (Functor m, MonadIO m) => GlanceConfig -> ActionM m ()
 imageSchemaH c = do
-  image_schema <- liftIO $ LBS.readFile "image_schema.json"
-  case (decode image_schema :: Maybe Value) of
-    Nothing ->
-      throwError $ S.stringError "Failed  to parse image_schema.json file"
-    Just v ->
-      S.json v
+  handleSchema "image_schema.json"
 
 metadefsNamespaceSchemaH :: (Functor m, MonadIO m)
                          => GlanceConfig -> ActionM m ()
 metadefsNamespaceSchemaH c = do
-  schema <- liftIO $ LBS.readFile "metadefs_namespace_schema.json"
+  handleSchema "metadefs_namespace_schema.json"
+
+metadefsNamespacesSchemaH :: (Functor m, MonadIO m)
+                         => GlanceConfig -> ActionM m ()
+metadefsNamespacesSchemaH c = do
+  handleSchema "metadefs_namespaces_schema.json"
+
+metadefsObjectSchemaH :: (Functor m, MonadIO m)
+                         => GlanceConfig -> ActionM m ()
+metadefsObjectSchemaH c = do
+  handleSchema "metadefs_object_schema.json"
+
+metadefsObjectsSchemaH :: (Functor m, MonadIO m)
+                         => GlanceConfig -> ActionM m ()
+metadefsObjectsSchemaH c = do
+  handleSchema "metadefs_objects_schema.json"
+
+metadefsPropertySchemaH :: (Functor m, MonadIO m)
+                         => GlanceConfig -> ActionM m ()
+metadefsPropertySchemaH c = do
+  handleSchema "metadefs_property_schema.json"
+
+metadefsPropertiesSchemaH :: (Functor m, MonadIO m)
+                         => GlanceConfig -> ActionM m ()
+metadefsPropertiesSchemaH c = do
+  handleSchema "metadefs_properties_schema.json"
+
+metadefsTagSchemaH :: (Functor m, MonadIO m)
+                         => GlanceConfig -> ActionM m ()
+metadefsTagSchemaH c = do
+  handleSchema "metadefs_tag_schema.json"
+
+metadefsTagsSchemaH :: (Functor m, MonadIO m)
+                         => GlanceConfig -> ActionM m ()
+metadefsTagsSchemaH c = do
+  handleSchema "metadefs_tags_schema.json"
+
+metadefsResourceTypeSchemaH :: (Functor m, MonadIO m)
+                         => GlanceConfig -> ActionM m ()
+metadefsResourceTypeSchemaH c = do
+  handleSchema "metadefs_resource_type_schema.json"
+
+metadefsResourceTypesSchemaH :: (Functor m, MonadIO m)
+                         => GlanceConfig -> ActionM m ()
+metadefsResourceTypesSchemaH c = do
+  handleSchema "metadefs_resource_types_schema.json"
+
+handleSchema :: (Functor m, MonadIO m) => FilePath -> ActionM m ()
+handleSchema filename = do
+  schema <- liftIO $ LBS.readFile filename
   case (decode schema :: Maybe Value) of
     Nothing ->
-      throwError $ S.stringError "Failed  to parse metadefs_namespace_schema.json file"
+      throwError $ S.stringError $ "Failed  to parse " ++ filename ++ " file"
     Just v ->
       S.json v
