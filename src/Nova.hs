f6a3a762f4d99e58add2bdc28bae7202d1e24e13
Victor Denisov <denisovenator@gmail.com>
2016-02-17 20:44:47 -0800

    Implement empty nova web app

--- src/Nova.hs
+++ src/Nova.hs
@@ -1,10 +1,94 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RecordWildCards #-}
 module Main
 where
 
-import Config (readConfig)
+import Common (loggerName)
+import Config (readConfig, ServerType(..))
+import Control.Exception (catch, SomeException)
+import Control.Monad.IO.Class (MonadIO(liftIO))
+import Data.ByteString.Lazy.Char8 (pack)
+import Data.Time.Clock (getCurrentTime)
+import Network.HTTP.Types.Status (statusCode, status500)
+import Network.Wai (Middleware, responseLBS)
+import Network.Wai.Handler.Warp (defaultSettings, setPort, runSettings)
+import Network.Wai.Handler.WarpTLS (tlsSettings, runTLS)
 import Nova.Config (confFileName, NovaConfig(..))
+import System.IO (stdout)
+import System.Log.Formatter (simpleLogFormatter)
+import System.Log.Handler (setFormatter)
+import System.Log.Handler.Simple (fileHandler, streamHandler)
+import System.Log.Logger ( setLevel, updateGlobalLogger, setHandlers
+                         , removeAllHandlers, debugM, noticeM, errorM)
+import Web.Common (ScottyM)
+
+import qualified Error as E
+import qualified Keystone.Web.Auth as A
+import qualified Keystone.Web.Auth.Types as AT
+import qualified Web.Scotty.Trans as S
 
 main = do
   (config :: NovaConfig) <- readConfig confFileName
-  return ()
+  let logFormatter = simpleLogFormatter "$utcTime (pid $pid, $tid) $prio: $msg"
+  stdoutHandler <- streamHandler stdout (logLevel config)
+  fileHandler <- fileHandler "nova.log" (logLevel config)
+  removeAllHandlers
+  updateGlobalLogger loggerName $ setLevel (logLevel config) . setHandlers
+    [ setFormatter stdoutHandler logFormatter
+    , setFormatter fileHandler   logFormatter
+    ]
+
+  -- Add verify database
+
+  !policy <- A.loadPolicy
+  -- ^ bang pattern is because we want to know if the policy is correct now
+  -- ^ we need the evaluation to happen immediatelly
+  verifyDatabase config
+
+  app <- S.scottyAppT id id (application policy config)
+
+  let settings = tlsSettings
+                      (certificateFile config)
+                      (keyFile config)
+
+  let serverSettings = setPort (port config) defaultSettings
+  noticeM loggerName $ "Starting web server @ port " ++ (show $ port config)
+  case serverType config of
+    Tls   -> runTLS settings serverSettings app
+    Plain -> runSettings serverSettings app
+
+application :: AT.Policy
+            -> NovaConfig
+            -> ScottyM IO ()
+application policy config = do
+  S.middleware exceptionCatchMiddleware
+  S.middleware logRequestResponse
+  S.defaultHandler $ \e -> do
+    S.status $ E.code e
+    case statusCode $ E.code e of
+      500 -> do
+        time <- liftIO $ getCurrentTime
+        liftIO $ errorM loggerName $ E.message e
+        S.json $ e {E.message = "Internal error. Server time - " ++ (show time)}
+      _ -> do
+        S.json e
+
+verifyDatabase :: NovaConfig -> IO ()
+verifyDatabase NovaConfig{..} = return () -- TODO
+
+exceptionCatchMiddleware :: Middleware
+exceptionCatchMiddleware app request responder = do
+  (app request responder)
+    `catch`
+      (\(e :: SomeException) -> do
+        time <- liftIO $ getCurrentTime
+        errorM loggerName $ show e
+        let message = "Internal error. Server time - " ++ (show time) ++ "\n"
+        responder $ responseLBS status500 [] (pack message)
+      )
+
+logRequestResponse :: Middleware
+logRequestResponse app request responder = do
+  debugM loggerName $ show request
+  app request responder
